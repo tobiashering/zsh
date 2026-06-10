c38db4f2fb4c10f7da832247af0bbfd6bb871c0f
Tobias Hering <tobias.hering96@gmx.de>
2026-06-10 23:21:32 +0200

    feat(scripts): refactor update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,57 +1,35 @@
 #!/usr/bin/env bash
 
-headline() {
-  gum style --bold --foreground 2 "$1"
-}
-
-update_apt() {
-  headline "Updating apt packages..."
-  sudo apt-get update
-  sudo apt-get upgrade
-  sudo apt-get autoremove
-  sudo apt-get clean
-  echo
-}
-
-update_snaps() {
-  if command -v snap &>/dev/null; then
-    headline "Updating snap packages"
-    sudo snap refresh
-    echo
-  fi
-}
-
-update_zsh() {
-  headline "Updating zsh repo"
-  git -C "$ZSH_FOLDER" stash
-  git -C "$ZSH_FOLDER" pull
-  echo
-}
-
-update_omz() {
-  headline "Updating ohmyzsh"
-  zsh -i -c "omz update"
-  echo
-}
-
-update_antidote() {
-  headline "Updating antidote plugins"
-  zsh -i -c "antidote update"
-  echo
-}
-
-update_mise() {
-  headline "Updating mise"
-  mise self-update
-  mise install
-  echo
-}
-
+#  Prompt for sudo password at the beginning of the script
 sudo -v
 
-update_apt
-update_snaps
-update_zsh
-update_omz
-update_antidote
-update_mise
+# Update APT packages
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get autoremove
+sudo apt-get clean
+echo
+
+# Update Snap packages
+if command -v snap &>/dev/null; then
+  sudo snap refresh
+  echo
+fi
+
+# Update Oh My Zsh repository
+git -C "$ZSH_FOLDER" stash
+git -C "$ZSH_FOLDER" pull
+echo
+
+# Update Oh My Zsh plugins and themes
+zsh -i -c "omz update"
+echo
+
+# Update Antidote plugins and themes
+zsh -i -c "antidote update"
+echo
+
+# Update Mise and dependencies
+mise self-update --yes
+mise install --yes
+echo
